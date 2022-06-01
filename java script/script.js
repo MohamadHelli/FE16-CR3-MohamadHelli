7061afc46d94d2e2d3f02164d74539b7279fa3b9
MohamadHelli <mou.hamad123@hotmail.com>
2022-06-01 10:56:02 +0200

    changes

--- java script/script.js
+++ java script/script.js
@@ -1,19 +1,19 @@
-let Haifa = 140;
-let Marcel = 20;
-let Hossam = 249;
-let Elissa = 140;
-let Cheb = 20;
-let Khaled = 249;
-let Tamer = 140;
-let Tarkan = 20;
-let Fadl = 249;
+let haifa = 140;
+let marcel = 20;
+let hossam = 249;
+let elissa = 140;
+let cheb = 20;
+let khaled = 249;
+let tamer = 140;
+let tarkan = 20;
+let fadl = 249;
 
-let sum = (Haifa + Marcel + Hossam + Elissa + Cheb + Khaled + Tamer + Tarkan + Fadl);
+let sum = (haifa + marcel + hossam + elissa + cheb + khaled + tamer + tarkan + fadl);
 console.log(sum);
 
-let fest_day1 = Haifa + Cheb + Fadl;
-let fest_day2 = Marcel + Khaled + Tarkan;
-let fest_day3 = Hossam + Elissa + Tamer;
+let fest_day1 = haifa + cheb + fadl;
+let fest_day2 = marcel + khaled + tarkan;
+let fest_day3 = hossam + elissa + tamer;
 console.log(fest_day1);
 console.log(fest_day2);
 console.log(fest_day3);
@@ -22,24 +22,24 @@ console.log(fest_day3);
 
     // First day
 
-    let Haifa = "Haifa";
-    let Cheb = "Cheb";
-    let Fadl = "Fadl";
-    console.log(`On the first day of the festival, I was lucky to watch bands  ${Haifa} , ${Cheb} and ${Fadl}. `)
+    let haifa = "Haifa";
+    let cheb = "Cheb";
+    let fadl = "Fadl";
+    console.log(`On the first day of the festival, I was lucky to watch bands  ${haifa} , ${cheb} and ${fadl}. `)
 
     // Second day
 
-    let Marcel = "Marcel";
-    let Khaled = "Khaled";
-    let Tarkan = "Tarkan";
-    console.log(`Then I watched the bands  ${Marcel} , ${Khaled} and ${Tarkan} on the second day and it cost me ${fest_day2}$ in total that day. `)
+    let marcel = "Marcel";
+    let khaled = "Khaled";
+    let tarkan = "Tarkan";
+    console.log(`Then I watched the bands  ${marcel} , ${khaled} and ${tarkan} on the second day and it cost me ${fest_day2}$ in total that day. `)
 
     // Third day
 
-    let Hossam = "Hossam";
-    let Elissa = "Elissa";
-    let Tamer = "Tamer";
-    console.log(`I was a bit tired on the third day but it was so much worth it to watch the bands  ${Hossam} , ${Elissa} and ${Tamer} and i spent ${fest_day3}$ for the three days of the festival. `)
+    let hossam = "Hossam";
+    let elissa = "Elissa";
+    let tamer = "Tamer";
+    console.log(`I was a bit tired on the third day but it was so much worth it to watch the bands  ${hossam} , ${elissa} and ${tamer} and i spent ${fest_day3}$ for the three days of the festival. `)
 
 }
 
